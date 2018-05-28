25375e1910026b1bd03e4379b37f849c6b81032e
exvalivar <varocm07@gmail.com>
2018-05-28 15:08:24 -0600

    se le agrega el botón en el código

--- app/src/main/java/com/admin/proyectos/appdesarrolladores/Login.java
+++ app/src/main/java/com/admin/proyectos/appdesarrolladores/Login.java
@@ -2,6 +2,9 @@ package com.admin.proyectos.appdesarrolladores;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("btnLogin","Presiono botón");
+                Log.w("btnLogin","warning");
+            }
+        });
     }
 }
