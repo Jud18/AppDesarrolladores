6fb72ec840554fa7054008b510fa21b0582267ac
Jud18 <jud_aramburo@hotmail.com>
2018-05-30 17:32:33 -0600

    Creación del item_category y el adapter para el uso de ListView.

--- app/src/main/java/com/admin/proyectos/appdesarrolladores/TabFragment01.java
+++ app/src/main/java/com/admin/proyectos/appdesarrolladores/TabFragment01.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class TabFragment01 extends Fragment {
 
