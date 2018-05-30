5b58967931251fa330c0cd99f2542215495035a9
Judith <jud_aramburo@hotmail.com>
2018-05-30 00:19:51 -0600

    Se agrega metodo para cambiar de pantalla al presionar botón de Login. Se crean los métodos necesarios para el cambio de Tabs.

--- app/src/main/java/com/admin/proyectos/appdesarrolladores/Home.java
+++ app/src/main/java/com/admin/proyectos/appdesarrolladores/Home.java
@@ -1,13 +1,73 @@
 package com.admin.proyectos.appdesarrolladores;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity{
+
+    FrameLayout simpleFrameLayout;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Get the reference of FrameLayout and TabLayout
+        simpleFrameLayout = (FrameLayout) findViewById(R.id.frame01);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame01, new TabFragment01());
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
+
+        // perform setOnTabSelectedListener event on TabLayout
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+            // get the current selected tab's position and replace the fragment accordingly
+                Fragment fragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        fragment = new TabFragment01();
+                        break;
+                    case 1:
+                        fragment = new TabFragment01();
+                        break;
+                    case 2:
+                        fragment = new TabFragment01();
+                        break;
+                }
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.frame01, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+
+    //Back Button do nothing
+    @Override
+    public void onBackPressed() {
     }
 }
--- app/src/main/java/com/admin/proyectos/appdesarrolladores/Login.java
+++ app/src/main/java/com/admin/proyectos/appdesarrolladores/Login.java
@@ -1,13 +1,27 @@
 package com.admin.proyectos.appdesarrolladores;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
